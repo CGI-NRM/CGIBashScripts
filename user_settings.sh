6255fa9b75524e986615896c341b5992c19574ac
wfenglund <72128748+wfenglund@users.noreply.github.com>
2024-05-15 22:36:29 +0200

    add placeholders for variables

--- user_settings.sh
+++ user_settings.sh
@@ -1,3 +1,3 @@
-hpc_user="" # your user on the hpc cluster
-hpc_project="" # your project name on the hpc cluster
-upload_link="" # link to your writeable folder on nextcloud
+hpc_user="edit_user_settings.sh" # your user on the hpc cluster
+hpc_project="edit_user_settings.sh" # your project name on the hpc cluster
+upload_link="edit_user_settings.sh" # link to your writeable folder on nextcloud
