10034298fbaf544ad0b51181c4ad8961b9abe7fb
wfenglund <72128748+wfenglund@users.noreply.github.com>
2024-06-21 00:23:49 +0200

    Create awsdownload_script.sh
    This script is a copy of the script of the same name under the private repository Naturhistoriska/eDNA.

---
+++ subscripts/awsdownload_script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# parse input file with new credentials:
+awsinfo=(`awk '{print $4}' input.txt`)
+
+# File that stores aws credentials:
+credentials_file=~/.aws/credentials
+
+# Save current credentials to backup if present:
+mkdir -p ~/.aws
+touch $credentials_file
+cat $credentials_file >> ~/.aws/credentials.bak
+
+# Write new credentials:
+echo "# "${awsinfo[0]} > $credentials_file
+echo "[default]" >> $credentials_file
+echo "aws_access_key_id = "${awsinfo[1]} >> $credentials_file
+echo "aws_secret_access_key = "${awsinfo[2]} >> $credentials_file
+
+# Download data from amazon S3:
+aws s3 cp s3://${awsinfo[0]} . --recursive
+
+# Get location of the md5 sums reported by BMK
+md5file=$(find . -iname '*md5*' -type f)
+
+datapath=$(dirname $md5file)
+# Check md5sums:
+(cd $datapath; md5sum --check data_md5.txt)
