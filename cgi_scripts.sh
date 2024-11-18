ead0c29e241a10c7e2507cbcf3886d3c77e9e8f0
wfenglund <w.f.englund@gmail.com>
2024-11-18 21:22:00 +0100

    added fqsttr script

--- cgi_scripts.sh
+++ cgi_scripts.sh
@@ -80,6 +80,29 @@ zhead () # print first 20 lines of a zipped file
   zcat $1 | head -n 20
 }
 
+fqsttr () # get some stats on any number of fastq files
+{
+  get_stats()
+  {
+    echo "- Sequence lengths:,"
+    zcat $1 | awk 'NR % 4 == 2' | awk '{print length}' | Rscript -e 'summary(scan("stdin", quiet = TRUE))'
+    count_seq=`zgrep "^@" ./$1 | wc -l`
+    echo "- Sequence count:,"
+    echo "    "$count_seq
+  }
+  counter=0
+  for arg # for every argument
+  do
+    if [ $counter -gt 0 ]
+    then
+      echo ""
+    fi
+    echo $arg
+    get_stats $arg | column -s ',' -t
+    counter=$((counter + 1))
+  done
+}
+
 ## HPC:
 
 ### Dardel:
