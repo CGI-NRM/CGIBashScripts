e9a40a44427f92a9abbcce740ae83906c2c2f982
wfenglund <w.f.englund@gmail.com>
2024-12-13 22:40:05 +0100

    clarified that project is referring to the compute project

--- .cgi_user_settings.sh
+++ .cgi_user_settings.sh
@@ -1,4 +1,4 @@
 hpc_user="edit_user_settings.sh" # your user on the hpc cluster
-hpc_project="edit_user_settings.sh" # your project name on the hpc cluster
+hpc_project="edit_user_settings.sh" # your compute project name on the hpc cluster
 upload_link="edit_user_settings.sh" # link to your writeable folder on nextcloud
 hpc_cluster="edit_user_settings.sh" # uppmax or dardel
